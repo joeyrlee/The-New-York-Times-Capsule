595dddeb85c2ea41c766e6d5152e3181ecb9e241
Joey Lee <jleepianist@gmail.com>
2017-02-20 12:06:03 -0800

    Added a random date generator method to app.js

--- app.js
+++ app.js
@@ -2,14 +2,34 @@
 //two-way search input data binding
 
 var app = new Vue({
-  el: '#search',
+  el: '#button',
   data: {
   	userInput: 'Enter a book name and press enter'
   },
   methods: {
-  	handleSearchInputChange: function(e) {
+  	getBook: function() {
 	  var currInput = this.userInput;
 	  console.log('this.userInput: ', this.userInput)
+		},
+		//generate a random date between June 2008 and now and return the string
+		generateRandomDate: function() {
+		  var date;
+		  while(!date){
+			  //the NYTimes API only has data from as early as June 2008
+			  var year = 2008 + Math.floor(Math.random() * 10);
+			  var month = Math.floor(Math.random() * 13);
+			  var day = Math.floor(Math.random() * 29);
+		  	
+		  	var randomDate = new Date(year,month,day)
+
+		  	//if the date is in the future OR before 2008-06-02
+			  if (randomDate > new Date() || randomDate < new Date(2008,06,02)) {
+			  	//reset the loop
+			  	date = undefined;
+			  } else {
+			  	return `${year}-${month}-${day}`;
+			  }
+		  }
+	  }
 	}
-  }
 });
