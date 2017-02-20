1d2ad6aada06395fcd41d5c10d55bf18f719d76c
Joey Lee <jleepianist@gmail.com>
2017-02-19 17:41:15 -0800

    Updated vue js file to facilitate the basic static html shell

---
+++ app.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+  el: '#search',
+  data: {
+  	message: 'Simply type in a book title and get back high-quality recommendations for similar reads!',
+  	userInput: 'Enter a book name here'
+  }
+})
