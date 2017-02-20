52cdbbad5cef2cf0b64c025e64122043a1860fc6
Joey Lee <jleepianist@gmail.com>
2017-02-20 12:50:45 -0800

    Corrected generateRandomDate month and day format

--- app.js
+++ app.js
@@ -4,21 +4,29 @@
 var app = new Vue({
   el: '#button',
   data: {
-  	userInput: 'Enter a book name and press enter'
+  	userInput: 'Type in a book name and press enter',
+  	books: {}
   },
   methods: {
+  	//call the NYTimes API and store/update the data in 
   	getBook: function() {
 	  var currInput = this.userInput;
-	  console.log('this.userInput: ', this.userInput)
+	  console.log(this.generateRandomDate())
 		},
-		//generate a random date between June 2008 and now and return the string
+		//generate a random date between June 2008 and now and return the formatted string
 		generateRandomDate: function() {
 		  var date;
 		  while(!date){
 			  //the NYTimes API only has data from as early as June 2008
 			  var year = 2008 + Math.floor(Math.random() * 10);
 			  var month = Math.floor(Math.random() * 13);
+			  if (String(month).length < 2) {
+			  	month = '0' + month;
+			  }
 			  var day = Math.floor(Math.random() * 29);
+			  if (String(day).length < 2) {
+			  	day = '0' + day;
+			  }
 		  	
 		  	var randomDate = new Date(year,month,day)
 
