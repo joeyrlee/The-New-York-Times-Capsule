21eb177590ed48921ab221ac5bafd6007ad8f284
Joey Lee <jleepianist@gmail.com>
2017-03-30 17:44:01 -0700

    (feat-update) Added a jQuery UI datepicker

---
+++ client/helpers/datePicker.js
@@ -0,0 +1,12 @@
+// Implement the jQuery UI datepicker on dom load
+$(function() {
+  $("#datepicker").datepicker({minDate: new Date(2008, 05, 08), maxDate: new Date()});
+});
+
+// Save the newly chosen date
+$(function() {
+	$('#datepicker').change(function(e) {
+		console.log(e.target.value);
+    app.$data.date = e.target.value;
+	})
+});
