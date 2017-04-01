2c94ae6e1b5f6f3cf40cb10ddb70fc448360d4e9
Joey Lee <jleepianist@gmail.com>
2017-04-01 10:43:54 -0700

    (feat-update) Incorporated custom date input to server request handler also making code DRYer with a single handler function

--- server/requestHandler.js
+++ server/requestHandler.js
@@ -2,22 +2,28 @@ var axios = require('axios');
 var API_KEY = require('./config/config.js');
 var generateRandomDate = require('./randGenerator.js');
 
-var handleRequest = function(listName, res) {
-	var date = generateRandomDate();
-	//ajax call to NYTimes API with the randomly generated date, list name/type, and api key
-	axios.get('http://api.nytimes.com/svc/books/v3/lists/' 
+var handleRequest = function(listName, res, date) {
+	var isRand = date ? false : true;
+	//if date is undefined, generate a random one
+	//for pseudo-random book recommendations
+	date = date || generateRandomDate();
+
+	axios.get('http://api.nytimes.com/svc/books/v3/lists/'
 		+ date + '/' + listName + '.json?api-key=' + API_KEY)
-	.then(function (response) {
+	.then(function(response) {
 		res.send(response.data);
 	})
-	.catch(function (error) {
+	.catch(function(error) {
 		console.log('*************************')
 		console.log('api request error: ', error);
 		console.log('*************************')
-		console.log('for selected option: ', listName);
-		console.log('for generated date: ', date);
-		res.send(`Uh-oh! There seems to have been an error retrieving the data for the "${listName}" search. Please try again!`)
-	});
+		console.log('for selected category: ', listName);
+		console.log('for date: ', date);
+		//change the error response message if the request was to be random for greater specificity
+		isRand
+		  ? res.send(`Uh-oh! There seems to have been an error retrieving the user-selected data for the "${listName}" search. Please try again!`)
+		  : res.send(`Uh-oh! There seems to have been an error retrieving the pseudo-random data for the "${listName}" search. Please try again!`)
+	})
 }
 
 module.exports = handleRequest;
