6acc28b7603c7629b0d793cb03a42e2a9fd660ae
joeyrlee <jleepianist@gmail.com>
2017-04-12 11:02:15 -0700

    (feat-update) Added gzip compression middleware for static file delivery

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var path = require('path');
+var compression = require('compression');
 var app = express();
 var handleRequest = require('./requestHandler.js');
 
+app.use(compression())
 app.use(express.static(path.join(__dirname, '../client')));
 
 app.get('/newbooks/:date/:listName', function ({params}, res) {
@@ -13,6 +15,4 @@ app.get('/newbook/:listName', function ({params}, res) {
   handleRequest(params.listName, res);
 })
 
-app.listen(3000, function () {
-  console.log('app listening on port 3000');
-})
+app.listen(3000);
