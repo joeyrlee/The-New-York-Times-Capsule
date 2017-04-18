626104b436625b7d4c62f9b4a4395abe86bb7eb0
joeyrlee <jleepianist@gmail.com>
2017-04-18 14:58:31 -0700

    (feat-update) Added/updated webpack entry file to better adhere to idiomatic Vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
-
-
-
 require('./styles.css');
-import App from './components/App.vue';
+import App from './components/App';
+import book from './components/Book.vue';
+import shareDialog from './helpers/assortedMethods.js';
+import {
+	handleSuccess,
+	handleError,
+	getBooks,
+	getRandBook,
+	encodeListName
+} from './helpers/vueMethods.js';
 
-const app = window.app = new Vue({
-  el: '#app',
-  // components: { App },
-});
+window.vue = new Vue({
+	el: '#vue',
+	components: {
+		book
+	},
+	data: {
+  	selectedCategory: '',
+    date: '',
+    books: []
+  },
+  template: App
+})
