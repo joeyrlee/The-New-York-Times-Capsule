f29a554dde9055bc391c9886b0aed1c42a909eff
joeyrlee <jleepianist@gmail.com>
2017-04-18 14:54:51 -0700

    (feat-update) Added shareDialog variable to global context

--- src/helpers/assortedMethods.js
+++ src/helpers/assortedMethods.js
@@ -1,7 +1,7 @@
 /* Function for handling modeless share dialogs */
 const shareDialog = function(url) {
-	if (app.$data.date) {
-		var date = app.$data.date;
+	if (vue.$data.date) {
+		var date = vue.$data.date;
 		//re-format the date string from month-day-year to year-month-day
 		date = date.slice(-4) + '/'	+ date.slice(0,2) + '/' + date.slice(3,5) + '/';
 	} else {
@@ -15,4 +15,5 @@ const shareDialog = function(url) {
 	return false;
 }
 
+window.shareDialog = shareDialog;
 export default shareDialog;
