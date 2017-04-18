cc33e882de21a99d630cda9160f5033995c61de7
joeyrlee <jleepianist@gmail.com>
2017-04-18 14:56:49 -0700

    (feat-update) Changed vue instance name to `vue` from `app`. Also added added helper methods to global scope

--- src/helpers/vueMethods.js
+++ src/helpers/vueMethods.js
@@ -1,27 +1,27 @@
 //dry success response handler
-export const handleSuccess = function(response, numBooks) {
+const handleSuccess = function(response, numBooks) {
 	//update our books variable with the number of books we wish to display
-	app.$data.books = response.data.results.books.slice(0, numBooks);
+	vue.$data.books = response.data.results.books.slice(0, numBooks);
 };
 
 //dry ajax error handler
-export const handleError = function(error) {
-  alert(`Uh-oh! There seems to have been an error retrieving the data for the ${app.$data.selectedCategory} search. Please try again!`)
+const handleError = function(error) {
+  alert(`Uh-oh! There seems to have been an error retrieving the data for the ${vue.$data.selectedCategory} search. Please try again!`)
 };
 
 //make ajax request to our server
-export const getBooks = function() {
-	axios.get('/newbooks/' + this.date.slice(-4) + '-'
+const getBooks = function() {
+	axios.get('http://localhost:3000/newbooks/' + vue.date.slice(-4) + '-'
 		//re-format the date string from month-day-year to year-month-day
-		+ this.date.slice(0,2) + '-' + this.date.slice(3,5)
-	  + '/' + encodeListName(this.selectedCategory))
+		+ vue.date.slice(0,2) + '-' + vue.date.slice(3,5)
+	  + '/' + encodeListName(vue.selectedCategory))
   .then(function(response){ handleSuccess(response, 5) })
   .catch(function(error){ handleError(error) })
 }
 
 //make ajax request to our server
-export const getRandBook = function() {
-	axios.get('/newbook/' + encodeListName(this.selectedCategory))
+const getRandBook = function() {
+	axios.get('http://localhost:3000/newbook/' + encodeListName(vue.selectedCategory))
   .then(function(response){
   	handleSuccess(response, 1)
   })
@@ -30,7 +30,7 @@ export const getRandBook = function() {
 
 //function to transform the english-formatted selected category 
 //to the api-friendly encoded format
-export const encodeListName = function(listName) {
+const encodeListName = function(listName) {
 	switch(listName) {
 		case 'Combined Print & E-Book Fiction':
 			return 'combined-print-and-e-book-fiction';
@@ -63,8 +63,15 @@ export const encodeListName = function(listName) {
 	}
 }
 
-// window.handleSuccess = handleSuccess;
-// window.handleError = handleError;
-// window.getBooks = getBooks;
-// window.getRandBook = getRandBook;
-// window.encodeListName = encodeListName;
+window.handleSuccess = handleSuccess;
+window.handleError = handleError;
+window.getBooks = getBooks;
+window.getRandBook = getRandBook;
+window.encodeListName = encodeListName;
+export {
+	handleSuccess,
+	handleError,
+	getBooks,
+	getRandBook,
+	encodeListName
+}
