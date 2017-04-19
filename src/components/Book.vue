2b4d432742ed4ac849d527aca2ed0daf98d391cf
joeyrlee <jleepianist@gmail.com>
2017-04-18 18:36:43 -0700

    (feat-update) Refactored Book template into a Vue single-file component, passing it `book` props from parent

---
+++ src/components/Book.vue
@@ -0,0 +1,20 @@
+<template>
+	<li class="book">
+		<div class="book-rank">{{book.rank}}</div>
+		<div class="img-box">
+	  	<img class="book-image" :src="book.book_image" />
+	  </div>
+		<div class="book-content">
+		  <div class="book-title">{{book.title}}</div>
+		  <div class="book-author">by {{book.author}}</div>
+		  <div class="book-description">{{book.description}}</div>
+		</div>
+	</li>
+</template>
+
+<script>
+	export default {
+		name: 'book',
+		props: ['book']
+	}
+</script>
