8eb6ec23616bd5260c2cfd50d98ae71c4d171ef2
joeyrlee <jleepianist@gmail.com>
2017-06-14 23:33:11 -0700

    (feat-update) Enhanced http caching

--- server/server.js
+++ server/server.js
@@ -4,15 +4,20 @@ const path = require('path');
 const compression = require('compression');
 const handleRequest = require('./requestHandler.js');
 
+/* gzip (compress) static files before delivering to users */
+app.use(compression());
+app.use(express.static(path.join(__dirname, '../dist')));
+
 app.use(function(req, res, next) {
+  /* Enable CORS */
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  /* Enable Caching */
+  res.header('Cache-Control', 'public, max-age=31557600');
+  res.header('Last-Modified', 'public, max-age=31557600');
   next();
 });
 
-//gzip (compress) static files before delivering to users
-app.use(compression());
-app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('/newbooks/:date/:listName', function ({params}, res) {
   handleRequest(params.listName, res, params.date);
